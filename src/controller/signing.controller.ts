d74a72e86dff09f35d11690046d8c1701f9619f7
Vrishin Patel <vrishin_patel@outlook.com>
2025-02-08 18:02:41 +0530

    implement /signpdf endpoint

--- src/controller/signing.controller.ts
+++ src/controller/signing.controller.ts
@@ -2,12 +2,30 @@ import {Elysia} from "elysia";
 import {SigningModel} from "../model/signing.model";
 import {SigningService} from '../service/signing.service'
 
-export const SigningController = new Elysia().use(SigningModel).post(
-	'/uploadpfx',
-	async ({ body }) => {
-		return SigningService.storePfx(body.file);
-	},
-	{
-		body: 'signing.uploadpfx.request.body',
-	},
-);
+export const SigningController = new Elysia()
+	.use(SigningModel)
+	.post(
+		'/uploadpfx',
+		async ({ body }) => {
+			return SigningService.storePfx(body.file);
+		},
+		{
+			body: 'signing.uploadpfx.request.body',
+		},
+	)
+	.post(
+		'/signpdf',
+		async ({ set, body }) => {
+			const signedPdf = await SigningService.signPdf(
+				body.file,
+				body.pfxId,
+				body.options,
+			);
+
+			set.headers['content-type'] = 'application/pdf';
+			return signedPdf;
+		},
+		{
+			body: 'signing.signpdf.request.body',
+		},
+	);
