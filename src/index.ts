7b5c472eb36364b49114a490c73aeaee5eaea91d
Vrishin Patel <vrishin_patel@outlook.com>
2025-02-08 15:20:20 +0530

    add biome for code formatting and linting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { Elysia } from "elysia";
+import {swagger} from '@elysiajs/swagger';
+import {Elysia} from 'elysia';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(swagger())
+	.get('/', () => 'Hello, World!')
+	.listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log('Server started');
+
+process.on('SIGINT', () => {
+	console.log('Server shutting down!');
+	process.exit(0);
+});
